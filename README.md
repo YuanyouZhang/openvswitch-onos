ac24188304f89beea68cb38bacb0c311537edc1a
wuwenbin2 <wuwenbin2@huawei.com>
2015-12-31 11:42:04 +0800

    openvswitch-onos

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# openvswitch-onos
-openvswitch-onos
+# Overview
 
-openvswitch-onos
+This subordinate charm provides OpenvSwitch integration with an ONOS controller.
+
+Its design to support deployment of principle charms as part of an ONOS SDN deployment.
+
+It also optionally supports deployment and configuration with OpenStack Compute (Nova).
+
+# Usage
+
+To deploy (partial deployment only - see other charms for full details):
+
+    juju deploy openvswitch-onos
+    juju deploy onos-controller
+    juju add-relation onos-controller openvswitch-onos
+
+
+This charm can be used with any other principle charm:
+
+    juju deploy ubuntu
+    juju add-relation openvswitch-onos ubuntu
+
+or with the OpenStack nova-compute and neutron-gateway charms:
+
+    juju deploy nova-compute
+    juju deploy neutron-gateway
+    juju add-relation nova-compute openvswitch-onos
+    juju add-relation neutron-gateway openvswitch-onos
+
+# Configuration Options
+
+This charm will optionally configure the local ip address of the OVS instance to something other than the 'private-address' provided by Juju:
+
+    juju set openvswitch-onos os-data-network=10.20.3.0/21
+
+The charm will scan configured network interfaces, and reconfigure the OVS instance with an alternative IP address if one is found within the configure subnet CIDR.
+
+# Restrictions
+
+This charm can't be deployed under LXC containers; however it will work just fine under KVM or on bare metal.
---
+++ hooks/reactive/main.py
@@ -0,0 +1,99 @@
+import subprocess
+
+from socket import gethostname
+
+import lib.ovs as ovs
+
+from charmhelpers.contrib.network.ip import get_address_in_network
+from charmhelpers.core.hookenv import config
+from charmhelpers.core.hookenv import log
+from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.hookenv import unit_private_ip
+from charmhelpers.core.reactive import hook
+from charmhelpers.core.reactive import when
+from charmhelpers.core.reactive import when_not
+from charmhelpers.core.unitdata import kv
+
+from charmhelpers.fetch import apt_install
+from charmhelpers.fetch import apt_purge
+from charmhelpers.fetch import filter_installed_packages
+
+# Packages to install/remove
+PACKAGES = ['openvswitch-switch']
+
+
+@when('ovsdb-manager.access.available')
+def configure_openvswitch(onos_ovsdb):
+    db = kv()
+    # NOTE(jamespage): Check connection string as well
+    #                  broken/departed seems busted right now
+    if db.get('installed') and onos_ovsdb.connection_string():
+        log("Configuring OpenvSwitch with ONOS controller: %s" %
+            onos_ovsdb.connection_string())
+        local_ip = get_address_in_network(config('os-data-network'),
+                                          unit_private_ip())
+        ovs.set_manager(onos_ovsdb.connection_string())
+        status_set('active', 'Open vSwitch configured and ready')
+
+
+@when_not('ovsdb-manager.access.available')
+def unconfigure_openvswitch(onos_ovsdb=None):
+    db = kv()
+    if db.get('installed'):
+        log("Unconfiguring OpenvSwitch")
+        subprocess.check_call(['ovs-vsctl', 'del-manager'])
+        bridges = subprocess.check_output(['ovs-vsctl',
+                                           'list-br']).split()
+        for bridge in bridges:
+            subprocess.check_call(['ovs-vsctl',
+                                   'del-controller', bridge])
+        status_set('waiting',
+                   'Open vSwitch not configured with ONOS controller')
+
+
+@when_not('ovsdb-manager.connected')
+def no_ovsdb_manager(onos_ovsdb=None):
+    status_set('blocked', 'Not related to ONOS controller')
+
+
+@when('neutron-plugin.connected')
+def configure_neutron_plugin(neutron_plugin):
+    neutron_plugin.configure_plugin(
+        plugin='onos',
+        config={
+            "nova-compute": {
+                "/etc/nova/nova.conf": {
+                    "sections": {
+                        'DEFAULT': [
+                            ('firewall_driver',
+                             'nova.virt.firewall.'
+                             'NoopFirewallDriver'),
+                            ('libvirt_vif_driver',
+                             'nova.virt.libvirt.vif.'
+                             'LibvirtGenericVIFDriver'),
+                            ('security_group_api', 'neutron'),
+                        ],
+                    }
+                }
+            }
+        })
+
+
+@hook('install')
+def install_packages():
+    db = kv()
+    if not db.get('installed'):
+        status_set('maintenance', 'Installing packages')
+        apt_install(filter_installed_packages(PACKAGES))
+        db.set('installed', True)
+
+
+@hook('stop')
+def uninstall_packages():
+    db = kv()
+    if db.get('installed'):
+        status_set('maintenance', 'Purging packages')
+        apt_purge(PACKAGES)
+        db.unset('installed')
+
+
